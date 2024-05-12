3d4eda5dacfadfdf3caa51c8b54c1c624767d0a8
“Hatuongnguyen” <hatuongnguyen0107@gmail.com>
2024-05-12 23:43:36 +0700

    Changing vector format

--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,74 @@
 #include "main.hpp"
 #include "kDTree.hpp"
 
+// tree.insert({5, 6}) syntax not support for macOS clang
+
 void tc1() {
     kDTree tree(2);
-    tree.insert({5, 6});
-    tree.insert({2, 2});
-    tree.insert({7, 3});
-    tree.insert({2, 8});
-    tree.insert({8, 7});
-    tree.insert({8, 1});
-    tree.insert({9, 4});
-    tree.insert({3, 5});
+    tree.insert(vector<int>({5, 6}));
+    tree.insert(vector<int>({2, 2}));
+    tree.insert(vector<int>({7, 3}));
+    tree.insert(vector<int>({2, 8}));
+    tree.insert(vector<int>({8, 7}));
+    tree.insert(vector<int>({8, 1}));
+    tree.insert(vector<int>({9, 4}));
+    tree.insert(vector<int>({3, 5}));
     tree.inorderTraversal();
 }
 
 void tc2() {
     kDTree tree(2);
-    tree.insert({5, 6});
-    tree.insert({2, 2});
-    tree.insert({7, 3});
-    tree.insert({2, 8});
-    tree.insert({8, 7});
-    tree.insert({8, 1});
-    tree.insert({9, 4});
-    tree.insert({3, 5});
-    tree.insert({9, 2});
+    tree.insert(vector<int>({5, 6}));
+    tree.insert(vector<int>({2, 2}));
+    tree.insert(vector<int>({7, 3}));
+    tree.insert(vector<int>({2, 8}));
+    tree.insert(vector<int>({8, 7}));
+    tree.insert(vector<int>({8, 1}));
+    tree.insert(vector<int>({9, 4}));
+    tree.insert(vector<int>({3, 5}));
+    tree.insert(vector<int>({9, 2}));
     tree.preorderTraversal();
 }
 
-void tc3() {
-    kDTree tree(2);
-    vector<vector<int> > pointList = {{5, 6}, {2, 2}, {7, 3}, {2, 8}, {8, 7}, {8, 1}, {9, 4}, {3, 5}};
-    tree.buildTree(pointList);
-    tree.preorderTraversal();
-}
-
-void tc4() {
-    kDTree tree(2);
-    vector<vector<int> > pointList = {{5, 6}, {2, 2}, {7, 3}, {2, 8}, {8, 7}, {8, 1}, {9, 4}, {3, 5}};
-    tree.buildTree(pointList);
-    cout << tree.search({2, 2}) << endl;
-    cout << tree.search({9, 3}) << endl;
-}
+//void tc3() {
+//    kDTree tree(2);
+//    vector<vector<int> > pointList = {{5, 6}, {2, 2}, {7, 3}, {2, 8}, {8, 7}, {8, 1}, {9, 4}, {3, 5}};
+//    tree.buildTree(pointList);
+//    tree.preorderTraversal();
+//}
+//
+//void tc4() {
+//    kDTree tree(2);
+//    vector<vector<int> > pointList = {{5, 6}, {2, 2}, {7, 3}, {2, 8}, {8, 7}, {8, 1}, {9, 4}, {3, 5}};
+//    tree.buildTree(pointList);
+//    cout << tree.search({2, 2}) << endl;
+//    cout << tree.search({9, 3}) << endl;
+//}
 
 void tc5() {
     kDTree tree(2);
-    tree.insert({5, 6});
-    tree.insert({2, 2});
-    tree.insert({7, 3});
-    tree.insert({2, 8});
-    tree.insert({8, 1});
-    tree.insert({3, 5});
-    tree.insert({9, 2});
-    tree.remove({5, 6});
+    tree.insert(vector<int>({5, 6}));
+    tree.insert(vector<int>({2, 2}));
+    tree.insert(vector<int>({7, 3}));
+    tree.insert(vector<int>({2, 8}));
+    tree.insert(vector<int>({8, 1}));
+    tree.insert(vector<int>({3, 5}));
+    tree.insert(vector<int>({9, 2}));
+    tree.remove(vector<int>({5, 6}));
     tree.preorderTraversal();
 }
 
 void tc6() {
     kDTree tree(2);
-    tree.insert({5, 6});
-    tree.insert({2, 2});
-    tree.insert({7, 3});
-    tree.insert({2, 8});
-    tree.insert({3, 5});
-    tree.insert({8, 2});
-    tree.insert({8, 7});
-    tree.insert({9, 2});
-    tree.insert({9, 5});
+    tree.insert(vector<int>({5, 6}));
+    tree.insert(vector<int>({2, 2}));
+    tree.insert(vector<int>({7, 3}));
+    tree.insert(vector<int>({2, 8}));
+    tree.insert(vector<int>({3, 5}));
+    tree.insert(vector<int>({8, 2}));
+    tree.insert(vector<int>({8, 7}));
+    tree.insert(vector<int>({9, 2}));
+    tree.insert(vector<int>({9, 5}));
     kDTreeNode* best = nullptr;
     tree.nearestNeighbour({9, 3}, best);
     cout << "Nearest neighbour of (9, 3) is " << *best << endl;
@@ -74,15 +76,15 @@ void tc6() {
 
 void tc7() {
     kDTree tree(2);
-    tree.insert({5, 6});
-    tree.insert({2, 2});
-    tree.insert({7, 3});
-    tree.insert({2, 8});
-    tree.insert({3, 5});
-    tree.insert({8, 2});
-    tree.insert({8, 7});
-    tree.insert({9, 2});
-    tree.insert({9, 5});
+    tree.insert(vector<int>({5, 6}));
+    tree.insert(vector<int>({2, 2}));
+    tree.insert(vector<int>({7, 3}));
+    tree.insert(vector<int>({2, 8}));
+    tree.insert(vector<int>({3, 5}));
+    tree.insert(vector<int>({8, 2}));
+    tree.insert(vector<int>({8, 7}));
+    tree.insert(vector<int>({9, 2}));
+    tree.insert(vector<int>({9, 5}));
     vector<kDTreeNode*> bestList;
     tree.kNearestNeighbour({9, 3}, 5, bestList);
     cout << "5 Nearest neighbour of (9, 3) are: ";
@@ -131,7 +133,7 @@ void tc9() {
 }
 
 int main(int argc, const char* argv[]) {
-//    tc1();
+    tc1();
 
     return 0;
 }
